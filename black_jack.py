6b76338ac89055a41d935b49044c27aa55fde1ff
kaxiok <38771332+kaxiok@users.noreply.github.com>
2023-03-21 22:05:07 -0700

    Update
    testing small update

--- black_jack.py
+++ black_jack.py
@@ -1,6 +1,8 @@
 import os, random, string, math
 
-deck = [1,2,3,4,5,6,7,8,9,10,11,12,13]*4
+A = 1
+J=Q=K=10
+deck = [A,2,3,4,5,6,7,8,9,10,J,Q,K]*4
 
 def infDeckGame():
     print ("You have chosen an infinite deck!\n")
