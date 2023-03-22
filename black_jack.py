572d377b3047dee35c8a8bc63b774beed940dce3
kaxiok <38771332+kaxiok@users.noreply.github.com>
2023-03-21 22:53:40 -0700

    Update
    Settings to change ace value, draw cards and sum

--- black_jack.py
+++ black_jack.py
@@ -1,8 +1,11 @@
 import os, random, string, math
 
-A = 1
+Ace=1
 J=Q=K=10
-deck = [A,2,3,4,5,6,7,8,9,10,J,Q,K]*4
+deck = []
+
+# Intro
+print ("Welcome to Blackjack simulation, where we will test how often we bust based on predetermined policies!\n")
 
 def infDeckGame():
     print ("You have chosen an infinite deck!\n")
@@ -11,17 +14,49 @@ def stanDeckGame():
 
 def startGame():
     decktype = 0
-    print ("Welcome to Blackjack simulation, where we will test how often we bust based on predetermined policies!\n")
+    _ace = 0
+    global Ace  # needs to be defined here as global to change value of variable
+
+
+    #Player chooses whether Ace's value is 1 or 10
+    _ace = input("Select value of Ace, Choose [0] for a value of 1 or [1] for a value of 10 \n")
+    if _ace == "0":
+        Ace = 1
+        print("A is now ", Ace)
+    elif _ace == "1":
+        Ace = 10
+        print("A is now ", Ace)
+    else:
+        print("Incorrect input, Ace value will be defaulted to 1")
+    
+
+
     decktype = input( "Choose a deck type [I]nfinite or [S]tandard: \n")
     if decktype == "I" or decktype == "i":
         infDeckGame()
-        exit()
+        #exit()
     elif decktype == "S" or decktype == "s":
         stanDeckGame()
-        exit()
+        #exit()
     else:
         print("Incorrect input! Ending simulation!")
         exit()
 
+# Change settings prompt
+settings = input("Do you want to change settings[Y]? Default: Ace=1, No card replacement \n")
+if settings == "Y" or settings == "y":
+    startGame()
 
-startGame()
+# Create deck after settings
+deck = [Ace,2,3,4,5,6,7,8,9,10,J,Q,K]*4
+
+# Random  index
+pick1 = random.randint(0,len(deck)-1)
+pick2 = random.randint(0,len(deck)-1)
+
+# Select card based on index
+card1 = deck[pick1]
+card2 = deck[pick2]
+
+print(card1, card2)
+print("your sum is: ", card1+card2)
